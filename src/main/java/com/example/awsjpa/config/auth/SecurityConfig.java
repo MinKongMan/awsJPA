8d3e747268b047f605b43c3a269caebcf24bbfe4
강민광 <69151872+MinKongMan@users.noreply.github.com>
2022-10-12 04:26:03 +0900

    설정 코드에서 각 기능별로 설명

--- src/main/java/com/example/awsjpa/config/auth/SecurityConfig.java
+++ src/main/java/com/example/awsjpa/config/auth/SecurityConfig.java
@@ -10,23 +10,37 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final CustomOAuth2UserService customOAuth2UserService;
-
+    // 설정 코드
     @Override
     protected void configure(HttpSecurity http) throws Exception{
         http.csrf().disable()
-                .headers().frameOptions().disable()
+                .headers().frameOptions().disable()// h2-console 화면을 사용하기 위해 해당 옵션들을 disable시킴
                 .and()
-                    .authorizeRequests()
+                    .authorizeRequests()// URL별 권한 관리를 설정하는 옵션의 시작점
+                                        // authorizeRequests가 선언되어야만 antMatchers 옵션을 사용할 수 있다.
                     .antMatchers("/", "/css/**","/images/**","/js/**","/h2-console/**").permitAll()
+                    // 권한 관리 대상을 지정하는 옵션
+                    // URL, HTTP 메소드별로 관리가 가능
+                    // "/" 등 지정된 URL 들은 permitAll()옵션을 통해 전체 열람 권한을 줬음음
                     .antMatchers("/api/v1/**").hasRole(Role.USER.name())
+                    // "/api/v1/** 주소를 가진 API는 USER 권한을 가진 사람만 가능하도록 설정
                     .anyRequest().authenticated()
+                    // 설정된 값들 이외 나머지 URL 들을 나타냄
+                    // authenticated()을 추가하여 나머지 URL들은 모두 인증된 사용자들에게만 허용 -> 로그인된 사람
+
                 .and()
                     .logout()
                     .logoutSuccessUrl("/")
+                    // 로그아웃에 대한 진입점
+                    // 로그아웃 성공 시 "/"주소로 이동
                 .and()
                     .oauth2Login()
+                    // OAuth2 로그인 기능에 대한 여러 설정의 진입점
                     .userInfoEndpoint()
+                    // OAuth2 로그인 성공 이후 사용자 정보를 가져올 때의 설정들을 담당
                     .userService(customOAuth2UserService);
+                    //소셜 로그인 성공 시 후속 조치를 진행할 UserService 인터페이스의 구현체를 등록
+                    // 리소스 서버에서 사용자 정보를 가져온 상태에서 추가로 진행하고자 하는 기능을 명시
     }
 }
 
--- src/main/java/com/example/awsjpa/domain/user/Role.java
+++ src/main/java/com/example/awsjpa/domain/user/Role.java
@@ -6,8 +6,8 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum Role {
-    GUEST("ROLE_GUEST","손님"),
-    USER("ROLE_USER","일반 사용자");
+    GUEST("ROLE_GUEST","손님"), // 스프링 시큐리티에서는 권한 코드에 항상 ROLE_이 붙어야 한다.
+    USER("ROLE_USER","일반 사용자"); // 따라서 코드별 키 값을 ROLE_GUEST, ROLE_USER 등으로 지정
 
     private final String key;
     private final String title;
