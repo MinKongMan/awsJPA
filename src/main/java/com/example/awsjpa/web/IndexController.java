f14456f7b674ea89d335874c29e964248a2122b3
강민광 <69151872+MinKongMan@users.noreply.github.com>
2022-11-10 17:39:39 +0900

    IndexController에서 Session 불러오는거 -> @LoginUser로 대체

--- src/main/java/com/example/awsjpa/web/IndexController.java
+++ src/main/java/com/example/awsjpa/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.example.awsjpa.web;
 
+import com.example.awsjpa.config.auth.LoginUser;
 import com.example.awsjpa.config.auth.dto.SessionUser;
 import com.example.awsjpa.domain.user.UserRepository;
 import com.example.awsjpa.service.posts.PostsService;
@@ -19,12 +20,22 @@ public class IndexController {
 
     private final PostsService postsService;
     private final HttpSession httpSession;
-    @GetMapping("/")
-    public String index(Model model){ // Model : 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장할 수 있음
-                                    // 여기서는 postsService.findAllDesc()로 가져온 결과를 posts로 index.mustache에 전달
+//    @GetMapping("/") // 개선되기 전
+//    public String index(Model model){ // Model : 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장할 수 있음
+//                                    // 여기서는 postsService.findAllDesc()로 가져온 결과를 posts로 index.mustache에 전달
+//        model.addAttribute("posts",postsService.findAllDesc());
+//
+//        SessionUser user = (SessionUser) httpSession.getAttribute("user"); 개선 사항에서 이 부분이 사라질 예정
+//
+//        if(user!=null) {
+//            model.addAttribute("userName",user.getName());
+//        }
+//
+//        return "index";
+//    }
+    @GetMapping("/") // 개선된 후
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts",postsService.findAllDesc());
-
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if(user!=null) {
             model.addAttribute("userName",user.getName());
@@ -32,6 +43,7 @@ public class IndexController {
 
         return "index";
     }
+
 
     @GetMapping("/posts/save")
     public String posts(Model model){
