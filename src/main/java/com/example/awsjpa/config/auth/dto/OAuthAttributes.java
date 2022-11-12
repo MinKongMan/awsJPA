4b885fa29478ddf1dce8cf2640f42045aeeda409
강민광 <69151872+MinKongMan@users.noreply.github.com>
2022-11-12 23:30:59 +0900

    네이버 로그인 API 연동 1. properties에 각종 설정 2. 네이버 로그인 버튼 생성 3. 네이버 생성자 생성 및 판단 코드 작성

--- src/main/java/com/example/awsjpa/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/example/awsjpa/config/auth/dto/OAuthAttributes.java
@@ -30,6 +30,9 @@ public class OAuthAttributes {
 
     // OAuth2User에서 반환하는 사용자 정보는 Map이기 때문에 값 하나하나를 변환해야만 한다.
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String,Object> attributes){
+        if("naver".equals(registrationId)){ // 네이버인지 판단하는 코드 추가
+            return ofNaver("id",attributes);
+        }
         return ofGoogle(userNameAttributeName,attributes);
     }
 
@@ -52,6 +55,20 @@ public class OAuthAttributes {
                 .email(email)
                 .picture(picture)
                 .role(Role.GUEST)
+                .build();
+    }
+
+
+    // 네이버 생성자 추가
+    public static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes){
+        Map<String, Object> response = (Map<String,Object>) attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String)response.get("name"))
+                .email((String)response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
                 .build();
     }
 }
